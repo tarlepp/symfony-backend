1c67245b02ac46b4be8fd5e173284d6586d4a090
tarlepp <tarmo.leppanen@kase.fi>
2016-04-06 20:03:36 +0300

    Added support to configure allowed IP addresses which can access application in development mode.

---
+++ app/config/development_ip_addresses.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file contains all base IP addresses that are allowed to access to application via development environment.
+ *
+ * Note that you can add your own IP addresses to 'development_ip_addresses_local.php' file which is ignored in VCS.
+ * Just create this file with following content:
+ *
+ * <code>
+ *  <?php
+ *
+ *  return [
+ *      'Your IP address here',
+ *      'Your another IP address here',
+ *  ];
+ * </code>
+ *
+ * @category    Config
+ * @package     App
+ * @author      TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+
+$configFile = __DIR__ . '/development_ip_addresses_local.php';
+
+// Get local IP addresses
+$local = file_exists($configFile) ? require_once $configFile : [];
+
+// By default allow "localhost"
+$base = [
+    '127.0.0.1',
+    'fe80::1',
+    '::1',
+];
+
+return array_merge($base, $local);
--- web/app_dev.php
+++ web/app_dev.php
@@ -19,17 +19,20 @@ use Symfony\Component\HttpFoundation\Request;
  * umask(0000);
  */
 
+// Get allowed IP addresses
+$allowedAddress = require_once __DIR__ . '/../app/config/development_ip_addresses.php';
+
 /**
  * This check prevents access to debug front controllers that are deployed by accident to production servers.
  * Feel free to remove this, extend it, or make something more sophisticated.
  */
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], $allowedAddress) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
 
-    exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
+    exit('You are not allowed to access this file.');
 }
 
 /**
@@ -44,12 +47,16 @@ Debug::enable();
 ErrorHandler::register();
 ExceptionHandler::register();
 
+// Create new AppKernel
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 
+// Create new request
 $request = Request::createFromGlobals();
 
+// Create response and send it back
 $response = $kernel->handle($request);
 $response->send();
 
+// Terminate current request/response cycle
 $kernel->terminate($request, $response);
