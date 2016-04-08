de801c3c92bf8de66471a3f4982eeeb292c6dd01
tarlepp <tarmo.leppanen@kase.fi>
2016-04-08 23:15:10 +0300

    Implemented UTC datetime to override some doctrine default.

---
+++ src/App/Doctrine/DBAL/Types/UTCDateTimeType.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * /src/App/Doctrine/DBAL/Types/UTCDateTimeType.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Doctrine\DBAL\Types;
+
+// Doctrine components
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\DateTimeType;
+
+/**
+ * Class UTCDateTimeType
+ *
+ * @see http://doctrine-orm.readthedocs.org/en/latest/cookbook/working-with-datetime.html
+ *
+ * @category    Doctrine
+ * @package     App\Doctrine\DBAL\Types
+ * @author      TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class UTCDateTimeType extends DateTimeType
+{
+    /**
+     * UTC date time zone object.
+     *
+     * @var \DateTimeZone
+     */
+    static private $utc;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if ($value instanceof \DateTime) {
+            $value->setTimezone(new \DateTimeZone('UTC'));
+        }
+
+        return parent::convertToDatabaseValue($value, $platform);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if (null === $value || $value instanceof \DateTime) {
+            return $value;
+        }
+
+        $converted = \DateTime::createFromFormat(
+            $platform->getDateTimeFormatString(),
+            $value,
+            self::$utc ? self::$utc : self::$utc = new \DateTimeZone('UTC')
+        );
+
+        if (!$converted) {
+            throw ConversionException::conversionFailedFormat(
+                $value,
+                $this->getName(),
+                $platform->getDateTimeFormatString()
+            );
+        }
+
+        return $converted;
+    }
+}
