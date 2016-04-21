17d03ceb2ec8bb975fb7cc3d0dac1f971be66c78
tarlepp <tarmo.leppanen@kase.fi>
2016-04-21 21:21:11 +0300

    Implemented serialize and unserialize.

--- src/App/Repository/User.php
+++ src/App/Repository/User.php
@@ -9,6 +9,12 @@ namespace App\Repository;
 // Application entities
 use App\Entity;
 
+// Symfony components
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
 /**
  * Doctrine repository class for User entities.
  *
@@ -16,7 +22,53 @@ use App\Entity;
  * @package     App\Repository
  * @author      TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
  */
-class User extends Base
+class User extends Base implements UserProviderInterface
 {
-    // Implement custom entity query methods here
+    /**
+     * Loads the user for the given username.
+     *
+     * This method must throw UsernameNotFoundException if the user is not
+     * found.
+     *
+     * @param string $username The username
+     *
+     * @return UserInterface
+     *
+     * @throws UsernameNotFoundException if the user is not found
+     */
+    public function loadUserByUsername($username)
+    {
+        // TODO: Implement loadUserByUsername() method.
+    }
+
+    /**
+     * Refreshes the user for the account interface.
+     *
+     * It is up to the implementation to decide if the user data should be
+     * totally reloaded (e.g. from the database), or if the UserInterface
+     * object can just be merged into some internal array of users / identity
+     * map.
+     *
+     * @param UserInterface $user
+     *
+     * @return UserInterface
+     *
+     * @throws UnsupportedUserException if the account is not supported
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        // TODO: Implement refreshUser() method.
+    }
+
+    /**
+     * Whether this provider supports the given user class.
+     *
+     * @param string $class
+     *
+     * @return bool
+     */
+    public function supportsClass($class)
+    {
+        // TODO: Implement supportsClass() method.
+    }
 }
