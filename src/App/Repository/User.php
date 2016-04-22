3ad8af71ea70f0a0208ade9895fe62cbff3a8244
tarlepp <tarmo.leppanen@kase.fi>
2016-04-22 20:55:46 +0300

    Implemented UserProviderInterface methods to User repository.

--- src/App/Repository/User.php
+++ src/App/Repository/User.php
@@ -9,6 +9,9 @@ namespace App\Repository;
 // Application entities
 use App\Entity;
 
+// Doctrine components
+use Doctrine\ORM\NoResultException;
+
 // Symfony components
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
@@ -27,48 +30,77 @@ class User extends Base implements UserProviderInterface
     /**
      * Loads the user for the given username.
      *
-     * This method must throw UsernameNotFoundException if the user is not
-     * found.
+     * This method must throw UsernameNotFoundException if the user is not found.
      *
-     * @param string $username The username
+     * Method is override for performance reasons see link below.
      *
-     * @return UserInterface
+     * @link http://symfony2-document.readthedocs.org/en/latest/cookbook/security/entity_provider.html#managing-roles-in-the-database
      *
-     * @throws UsernameNotFoundException if the user is not found
+     * @param   string  $username   The username
+     *
+     * @return  UserInterface
+     *
+     * @throws  UsernameNotFoundException if the user is not found
      */
     public function loadUserByUsername($username)
     {
-        // TODO: Implement loadUserByUsername() method.
+        // Build query
+        $query = $this
+            ->createQueryBuilder('u')
+            ->select('u, g')
+            ->leftJoin('u.user_groups', 'g')
+            ->where('u.username = :username OR u.email = :email')
+            ->setParameter('username', $username)
+            ->setParameter('email', $username)
+            ->getQuery()
+        ;
+
+        try {
+            $user = $query->getSingleResult();
+        } catch (NoResultException $exception) {
+            $message = sprintf(
+                'User "%s" not found',
+                $username
+            );
+
+            throw new UsernameNotFoundException($message);
+        }
+
+        return $user;
     }
 
     /**
      * Refreshes the user for the account interface.
      *
-     * It is up to the implementation to decide if the user data should be
-     * totally reloaded (e.g. from the database), or if the UserInterface
-     * object can just be merged into some internal array of users / identity
-     * map.
+     * It is up to the implementation to decide if the user data should be totally reloaded (e.g. from the database),
+     * or if the UserInterface object can just be merged into some internal array of users / identity map.
      *
-     * @param UserInterface $user
+     * @param   UserInterface   $user
      *
-     * @return UserInterface
+     * @return  UserInterface
      *
-     * @throws UnsupportedUserException if the account is not supported
+     * @throws  UnsupportedUserException    if the account is not supported
      */
     public function refreshUser(UserInterface $user)
     {
-        // TODO: Implement refreshUser() method.
+        $class = get_class($user);
+
+        if (!$this->supportsClass($class)) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', $class));
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
     }
 
     /**
      * Whether this provider supports the given user class.
      *
-     * @param string $class
+     * @param   string  $class
      *
-     * @return bool
+     * @return  bool
      */
     public function supportsClass($class)
     {
-        // TODO: Implement supportsClass() method.
+        return $this->getEntityName() === $class || is_subclass_of($class, $this->getEntityName());
     }
 }
