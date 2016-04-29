b7615f93722ef2118505d61c0ac0a34d7bc30545
tarlepp <tarmo.leppanen@kase.fi>
2016-04-30 00:39:29 +0300

    This seems stupid... but cannot  figure out any other way to do this now...

--- src/App/Controller/AuthorController.php
+++ src/App/Controller/AuthorController.php
@@ -7,10 +7,12 @@
 namespace App\Controller;
 
 // Symfony components
+use FOS\RestBundle\Context\Context;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 // 3rd party components
 use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class AuthorController
@@ -41,13 +43,52 @@ class AuthorController extends FOSRestController
     /**
      * todo
      *
+     * @param Request $request
+     *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function getAuthorAction()
+    public function getAuthorAction(Request $request)
     {
+        $populate = (array)$request->get('populate', []);
+        $populateAll = array_key_exists('populateAll', $request->query->all());
+
+        $context = $this->getSerializeContext($populate, $populateAll);
+
         $data = $this->repository->findAll();
         $view = $this->view($data, 200);
+        $view->setContext($context);
 
         return $this->handleView($view);
+    }
+
+    /**
+     * Helper method to get serialization context for query.
+     *
+     * @param   array   $populate
+     * @param   boolean $populateAll
+     *
+     * @return  Context
+     */
+    protected function getSerializeContext(array $populate, $populateAll)
+    {
+        $bits = explode('\\', $this->repository->getClassName());
+
+        // Determine used default group
+        $defaultGroup = $populateAll ? 'Default' : end($bits);
+
+        if (count($populate) === 0 && $populateAll) {
+            $associations = array_keys(
+                $this->getDoctrine()->getManager()->getClassMetadata('AppBundle:Author')->getAssociationMappings()
+            );
+
+            $populate = array_map('ucfirst', $associations);
+        }
+
+        // Create context and set used groups
+        $context = new Context();
+        $context->addGroups(array_merge([$defaultGroup], $populate));
+        $context->setSerializeNull(true);
+
+        return $context;
     }
 }
