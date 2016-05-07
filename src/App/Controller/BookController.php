f419e5a711849d7c1ee853b2e15cf1b61fd03ce9
tarlepp <tarmo.leppanen@kase.fi>
2016-05-07 16:56:41 +0300

    Implemented BookController actions.

--- src/App/Controller/BookController.php
+++ src/App/Controller/BookController.php
@@ -6,48 +6,146 @@
  */
 namespace App\Controller;
 
-// Symfony components
-use Symfony\Component\DependencyInjection\ContainerInterface;
+// Application components
+use App\Services\Book;
 
-// 3rd party components
-use FOS\RestBundle\Controller\FOSRestController;
+// Sensio components
+use /** @noinspection PhpUnusedAliasInspection */ Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use /** @noinspection PhpUnusedAliasInspection */ Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use /** @noinspection PhpUnusedAliasInspection */ Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+// Symfony components
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
- * Class BookController
+ * Class AuthorController
  *
- * @category    REST
+ * @Route("/book")
+ *
+ * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+ *
+ * @category    Controller
  * @package     App\Controller
  * @author      TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
  */
-class BookController extends FOSRestController
+class BookController extends Rest
 {
     /**
-     * @var \Doctrine\Common\Persistence\ObjectRepository
+     * Service object for controller.
+     *
+     * @var Book
      */
-    protected $repository;
+    protected $service;
 
     /**
-     * Sets the container.
+     * Name of the service that controller uses. This is used on setContainer method to invoke specified service to
+     * class context.
      *
-     * @param ContainerInterface|null $container A ContainerInterface instance or null
+     * @var string
      */
-    public function setContainer(ContainerInterface $container = null)
-    {
-        parent::setContainer($container);
+    protected $serviceName = 'app.services.book';
 
-        $this->repository = $this->getDoctrine()->getRepository('AppBundle:Book');
+    /**
+     * Route action to get array of authors.
+     *
+     * @Route("/")
+     *
+     * @Method({"GET"})
+     *
+     * @Security("has_role('ROLE_USER')")
+     *
+     * @param   Request $request
+     *
+     * @return  Response
+     */
+    public function find(Request $request)
+    {
+        return parent::find($request);
     }
 
     /**
-     * todo
+     * Route action to get specified author.
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route(
+     *      "/{id}",
+     *      requirements={"id" = "\d+"}
+     *  )
+     *
+     * @Method({"GET"})
+     *
+     * @Security("has_role('ROLE_USER')")
+     *
+     * @param   Request $request
+     * @param   integer $id
+     *
+     * @return  Response
      */
-    public function getBookAction()
+    public function findOne(Request $request, $id = 0)
     {
-        $data = $this->repository->findAll();
-        $view = $this->view($data, 200);
+        return parent::findOne($request, $id);
+    }
 
-        return $this->handleView($view);
+    /**
+     * Route action to create new author.
+     *
+     * @Route("")
+     * @Route("/")
+     *
+     * @Method({"POST"})
+     *
+     * @Security("has_role('ROLE_ADMIN')")
+     *
+     * @param   Request $request
+     *
+     * @return  Response
+     */
+    public function create(Request $request)
+    {
+        return parent::create($request);
+    }
+
+    /**
+     * Route action to update author data.
+     *
+     * @Route(
+     *      "/{id}",
+     *      requirements={"id" = "\d+"}
+     *  )
+     *
+     * @Method({"PUT"})
+     *
+     * @Security("has_role('ROLE_ADMIN')")
+     *
+     * @param   Request $request
+     * @param   integer $id
+     *
+     * @return  Response
+     */
+    public function update(Request $request, $id)
+    {
+        return parent::update($request, $id);
+    }
+
+    /**
+     * Route action to delete specified author.
+     *
+     * @Route(
+     *      "/{id}",
+     *      requirements={"id" = "\d+"}
+     *  )
+     *
+     * @Method({"DELETE"})
+     *
+     * @Security("has_role('ROLE_ADMIN')")
+     *
+     * @param   Request $request
+     * @param   integer $id
+     *
+     * @return  Response
+     */
+    public function delete(Request $request, $id)
+    {
+        return parent::delete($request, $id);
     }
 }
