70111f44a1cbaa54544fc44d172fe98f46de9b05
tarlepp <tarmo.leppanen@kase.fi>
2016-05-11 18:16:08 +0300

    Added controller for /user endpoint.

---
+++ src/App/Controller/UserController.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * /src/App/Controller/UserController.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Controller;
+
+// Application components
+use App\Services\Book;
+
+// Sensio components
+use /** @noinspection PhpUnusedAliasInspection */ Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use /** @noinspection PhpUnusedAliasInspection */ Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use /** @noinspection PhpUnusedAliasInspection */ Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+// Symfony components
+use /** @noinspection PhpUnusedAliasInspection */ Symfony\Component\HttpFoundation\Response;
+use /** @noinspection PhpUnusedAliasInspection */ Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class UserController
+ *
+ * @Route("/user")
+ *
+ * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+ *
+ * @category    Controller
+ * @package     App\Controller
+ * @author      TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class UserController extends Rest
+{
+    /**
+     * Service object for controller.
+     *
+     * @var Book
+     */
+    protected $service;
+
+    /**
+     * Name of the service that controller uses. This is used on setContainer method to invoke specified service to
+     * class context.
+     *
+     * @var string
+     */
+    protected $serviceName = 'app.services.user';
+
+    /**
+     * {@inheritdoc}
+     *
+     * @Route("/")
+     *
+     * @Method({"GET"})
+     *
+     * @Security("has_role('ROLE_ADMIN')")
+     *
+     * @param   Request     $request
+     *
+     * @return  Response
+     */
+    public function find(Request $request)
+    {
+        return parent::find($request);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @Route(
+     *      "/{id}",
+     *      requirements={"id" = "\d+"}
+     *  )
+     *
+     * @Method({"GET"})
+     *
+     * @Security("has_role('ROLE_ADMIN')")
+     *
+     * @param   Request $request
+     * @param   integer $id
+     *
+     * @return  Response
+     */
+    public function findOne(Request $request, $id)
+    {
+        parent::findOne($request, $id);
+    }
+}
