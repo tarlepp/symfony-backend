287b63f5c39219bfef19fec1b38127cb24b19318
tarlepp <tarmo.leppanen@kase.fi>
2016-05-19 19:36:49 +0300

    Added custom JMS Serializer handler for ArrayCollections.

---
+++ src/App/Serializer/Handler/ArrayCollectionHandler.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * /src/App/Serializer/Handler/ArrayCollectionHandler.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Serializer\Handler;
+
+use Doctrine\Common\Collections\Collection;
+use JMS\Serializer\Context;
+use JMS\Serializer\VisitorInterface;
+
+/**
+ * Class ArrayCollectionHandler
+ *
+ * @link        https://github.com/schmittjoh/JMSSerializerBundle/issues/373
+ *
+ * @category    Model
+ * @package     App\Serializer\Handler
+ * @author      TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class ArrayCollectionHandler extends \JMS\Serializer\Handler\ArrayCollectionHandler
+{
+    /**
+     * Method to fix serialization of ArrayCollection items.
+     *
+     * @param   VisitorInterface    $visitor
+     * @param   Collection          $collection
+     * @param   array               $type
+     * @param   Context             $context
+     *
+     * @return  mixed
+     */
+    public function serializeCollection(
+        VisitorInterface $visitor,
+        Collection $collection,
+        array $type,
+        Context $context
+    ) {
+        // We change the base type, and pass through possible parameters.
+        $type['name'] = 'array';
+
+        // Don't include items that will produce null elements
+        $dataArray = [];
+
+        foreach ($collection->toArray() as $element) {
+            if (!$context->isVisiting($element)) {
+                $dataArray[] = $element;
+            }
+        }
+
+        return $visitor->visitArray($dataArray, $type, $context);
+    }
+}
