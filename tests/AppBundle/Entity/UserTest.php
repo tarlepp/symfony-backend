a500e85cb7bc723ed0ffcca54ac3e5fcd9a99102
tarlepp <tarmo.leppanen@kase.fi>
2016-06-15 20:19:01 +0300

    Added test to cover user password hash.

--- tests/AppBundle/Entity/UserTest.php
+++ tests/AppBundle/Entity/UserTest.php
@@ -26,4 +26,31 @@ class UserTest extends EntityTestCase
      * @var string
      */
     protected $entityName = 'App\Entity\User';
+
+    /**
+     * @dataProvider dataProviderTestThatPasswordHashingIsWorkingAsExpected
+     *
+     * @param   callable    $callable
+     * @param   string      $password
+     * @param   string      $expected
+     */
+    public function testThatPasswordHashingIsWorkingAsExpected($callable, $password, $expected)
+    {
+        $this->entity->setPassword($callable, $password);
+
+        $this->assertEquals($expected, $this->entity->getPassword());
+    }
+
+    /**
+     * Data provider for testThatPasswordHashingIsWorkingAsExpected
+     *
+     * @return array
+     */
+    public function dataProviderTestThatPasswordHashingIsWorkingAsExpected()
+    {
+        return [
+            ['str_rot13', 'password', 'cnffjbeq'],
+            ['base64_encode', 'password', 'cGFzc3dvcmQ='],
+        ];
+    }
 }
