4942f477df8a9e4e1cd58d02180b53c8113e2967
tarlepp <tarmo.leppanen@kase.fi>
2016-06-16 21:24:05 +0300

    Added helper ContainerTestCase class.

---
+++ src/App/Tests/ContainerTestCase.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * /src/App/Tests/ContainerTestCase.php
+ *
+ * @User  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class ContainerTestCase
+ *
+ * @package App\Tests
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class ContainerTestCase extends KernelTestCase
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * Getter method for container
+     *
+     * @return ContainerInterface
+     */
+    public function getContainer()
+    {
+        if (!($this->container instanceof ContainerInterface)) {
+            self::bootKernel();
+
+            $this->container = static::$kernel->getContainer();
+        }
+
+        return $this->container;
+    }
+}
