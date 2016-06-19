ba377004719e6251c8bbd889cfd497db069de187
tarlepp <tarmo.leppanen@protacon.com>
2016-06-19 21:48:54 +0300

    Moar tests...

--- tests/AppBundle/Doctrine/Behaviours/TimestampableTest.php
+++ tests/AppBundle/Doctrine/Behaviours/TimestampableTest.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: wunder
- * Date: 19.6.2016
- * Time: 21:28
- */
 
 namespace AppBundle\Doctrine\Behaviours;
-
 
 use App\Entity\Author;
 use App\Tests\EntityTestCase;
@@ -22,24 +15,20 @@ class TimestampableTest extends EntityTestCase
     /**
      * @var string
      */
-    protected $entityName = 'App\Entity\Book';
-
-    public function setUp()
-    {
-        parent::setUp();
-
-        $this->entity->setCreatedAt(new \DateTime());
-        $this->entity->setUpdatedAt(new \DateTime());
-    }
+    protected $entityName = 'App\Entity\Author';
 
     public function testThatGetCreatedAtJsonReturnsExpected()
     {
+        $this->entity->setCreatedAt(new \DateTime());
+
         $this->assertInstanceOf('\DateTime', $this->entity->getCreatedAt());
         $this->assertEquals('UTC', $this->entity->getCreatedAt()->getTimezone()->getName());
     }
 
     public function testThatGetUpdatedAtJsonReturnsExpected()
     {
+        $this->entity->setUpdatedAt(new \DateTime());
+
         $this->assertInstanceOf('\DateTime', $this->entity->getUpdatedAt());
         $this->assertEquals('UTC', $this->entity->getUpdatedAt()->getTimezone()->getName());
     }
