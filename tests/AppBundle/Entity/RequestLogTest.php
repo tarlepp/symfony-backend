d695f44317d99225e4a09bc74fddb6eb60af5b34
tarlepp <tarmo.leppanen@protacon.com>
2016-06-30 18:50:10 +0300

    Added generic entity tests for RequestLog entity.

---
+++ tests/AppBundle/Entity/RequestLogTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * /tests/AppBundle/Entity/RequestLogTest.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace AppBundle\Entity;
+
+use App\Entity\RequestLog;
+use App\Tests\EntityTestCase;
+
+/**
+ * Class RequestLogTest
+ *
+ * @package AppBundle\Entity
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class RequestLogTest extends EntityTestCase
+{
+    /**
+     * @var RequestLog
+     */
+    protected $entity;
+
+    /**
+     * @var string
+     */
+    protected $entityName = 'App\Entity\RequestLog';
+}
