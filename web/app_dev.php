80c3bd0f50625689e3faf8f72b66627863a90c73
tarlepp <tarmo.leppanen@protacon.com>
2016-08-30 19:51:18 +0300

    Added support to access app_dev.php from any IP - Note this is not recommend use case...

--- web/app_dev.php
+++ web/app_dev.php
@@ -26,9 +26,13 @@ $allowedAddress = require_once __DIR__ . '/../app/config/development_ip_addresse
  * This check prevents access to debug front controllers that are deployed by accident to production servers.
  * Feel free to remove this, extend it, or make something more sophisticated.
  */
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], $allowedAddress) || php_sapi_name() === 'cli-server')
+if (!in_array('*', $allowedAddress)
+    && (
+        isset($_SERVER['HTTP_CLIENT_IP'])
+        || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+        || !(in_array(@$_SERVER['REMOTE_ADDR'], $allowedAddress)
+        || php_sapi_name() === 'cli-server')
+    )
 ) {
     header('HTTP/1.0 403 Forbidden');
 
