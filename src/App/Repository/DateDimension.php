7e6b1a89398f89e6de5202c458f19a8cb384eda0
tarlepp <tarmo.leppanen@protacon.com>
2016-11-23 21:57:16 +0200

    Added 'DateDimension' entity, repository and configuration for this.

---
+++ src/App/Repository/DateDimension.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/App/Repository/DateDimension.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Repository;
+
+/**
+ * Doctrine repository class for DateDimension entities.
+ *
+ * @package App\Repository
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class DateDimension extends Base
+{
+    // Implement custom entity query methods here
+}
