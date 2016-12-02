5b67a7d3dc2b8b9aec1b4d781571f2c186567f83
tarlepp <tarmo.leppanen@protacon.com>
2016-12-02 19:54:28 +0200

    Added base PHPSpec for UserController

---
+++ spec/App/Controller/UserControllerSpec.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * /spec/App/Controller/UserControllerSpec.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+declare(strict_types = 1);
+
+namespace spec\App\Controller;
+
+use App\Controller\UserController;
+use App\Services\Rest\Helper\Interfaces\Response as RestHelperResponseInterface;
+use App\Services\Rest\Interfaces\Base as ResourceServiceInterface;
+use PhpSpec\ObjectBehavior;
+use PhpSpec\Wrapper\Collaborator;
+
+/**
+ * Class UserControllerSpec
+ *
+ * @mixin UserController
+ *
+ * @package spec\App\Controller
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class UserControllerSpec extends ObjectBehavior
+{
+    /**
+     * @param   Collaborator|ResourceServiceInterface    $resourceService
+     * @param   Collaborator|RestHelperResponseInterface $restHelperResponse
+     */
+    public function let(
+        ResourceServiceInterface $resourceService,
+        RestHelperResponseInterface $restHelperResponse
+    ) {
+        $restHelperResponse->setResourceService($resourceService);
+
+        $this->beConstructedWith(
+            $resourceService,
+            $restHelperResponse
+        );
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(UserController::class);
+    }
+}
