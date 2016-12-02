39ee3e482f9700091d65097c4e1ddf723b72aa8b
tarlepp <tarmo.leppanen@protacon.com>
2016-12-02 19:53:09 +0200

    Added base PHPSpec for BookController

---
+++ spec/App/Controller/BookControllerSpec.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * /spec/App/Controller/BookControllerSpec.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+declare(strict_types = 1);
+
+namespace spec\App\Controller;
+
+use App\Controller\BookController;
+use App\Services\Rest\Helper\Interfaces\Response as RestHelperResponseInterface;
+use App\Services\Rest\Interfaces\Base as ResourceServiceInterface;
+use PhpSpec\ObjectBehavior;
+use PhpSpec\Wrapper\Collaborator;
+
+/**
+ * Class BookControllerSpec
+ *
+ * @mixin BookController
+ *
+ * @package spec\App\Controller
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class BookControllerSpec extends ObjectBehavior
+{
+    /**
+     * @param   Collaborator|ResourceServiceInterface    $resourceService
+     * @param   Collaborator|RestHelperResponseInterface $restHelperResponse
+     */
+    public function let(
+        ResourceServiceInterface $resourceService,
+        RestHelperResponseInterface $restHelperResponse
+    ) {
+        $restHelperResponse->setResourceService($resourceService);
+
+        $this->beConstructedWith(
+            $resourceService,
+            $restHelperResponse
+        );
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(BookController::class);
+    }
+}
