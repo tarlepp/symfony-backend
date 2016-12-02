e169cf3b843ea49df1fd7ed8767acf935bb10ede
tarlepp <tarmo.leppanen@protacon.com>
2016-12-02 20:19:39 +0200

    Added more spec cases.

--- spec/App/Controller/DefaultControllerSpec.php
+++ spec/App/Controller/DefaultControllerSpec.php
@@ -9,6 +9,7 @@ namespace spec\App\Controller;
 
 use App\Controller\DefaultController;
 use PhpSpec\ObjectBehavior;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class DefaultControllerSpec
@@ -23,5 +24,27 @@ class DefaultControllerSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType(DefaultController::class);
+    }
+
+    function it_should_response_to_index_action()
+    {
+        /** @var ObjectBehavior $response */
+        $response = $this->indexAction();
+
+        $response->shouldHaveType('Symfony\Component\HttpFoundation\Response');
+    }
+
+    function it_should_response_http_status_code_200_to_index_action()
+    {
+        $response = $this->indexAction();
+
+        $response->getStatusCode()->shouldReturn(200);
+    }
+
+    function it_should_response_empty_body_to_index_action()
+    {
+        $response = $this->indexAction();
+
+        $response->getContent()->shouldEqual('');
     }
 }
