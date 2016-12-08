83567dd70d1a4c822b2885a950f5e3ec1ce6916c
tarlepp <tarmo.leppanen@protacon.com>
2016-12-08 18:44:27 +0200

    Added more specs for App\Services\LoginLogger

--- spec/App/Services/LoginLoggerSpec.php
+++ spec/App/Services/LoginLoggerSpec.php
@@ -7,13 +7,16 @@ declare(strict_types = 1);
  */
 namespace spec\App\Services;
 
+use App\Entity\User as UserEntity;
 use App\Services\Interfaces\LoginLogger as LoginLoggerInterface;
 use App\Services\LoginLogger;
 use App\Services\Rest\UserLogin as UserLoginService;
-use Doctrine\ORM\EntityRepository;
+use App\Repository\User as UserRepository;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Class LoginLoggerSpec
@@ -26,13 +29,13 @@ class LoginLoggerSpec extends ObjectBehavior
     /**
      * @param   \PhpSpec\Wrapper\Collaborator|LoggerInterface   $logger
      * @param   \PhpSpec\Wrapper\Collaborator|UserLoginService  $userLoginService
-     * @param   \PhpSpec\Wrapper\Collaborator|EntityRepository  $userRepository
+     * @param   \PhpSpec\Wrapper\Collaborator|UserRepository    $userRepository
      * @param   \PhpSpec\Wrapper\Collaborator|RequestStack      $requestStack
      */
     function let(
         LoggerInterface $logger,
         UserLoginService $userLoginService,
-        EntityRepository $userRepository,
+        UserRepository $userRepository,
         RequestStack $requestStack
     ) {
         $this->beConstructedWith($logger, $userLoginService, $userRepository, $requestStack);
@@ -42,5 +45,31 @@ class LoginLoggerSpec extends ObjectBehavior
     {
         $this->shouldHaveType(LoginLogger::class);
         $this->shouldImplement(LoginLoggerInterface::class);
+    }
+
+    /**
+     * @param   \PhpSpec\Wrapper\Collaborator|UserInterface     $user
+     * @param   \PhpSpec\Wrapper\Collaborator|UserRepository    $userRepository
+     */
+    function it_should_return_expected_value_when_calling_setUser_method_without_user(
+        UserInterface $user,
+        UserRepository $userRepository
+    ) {
+        $userRepository->loadUserByUsername(Argument::any())->shouldBeCalled()->willReturn($user);
+
+        $this->setUser($user)->shouldReturn($this);
+    }
+
+    /**
+     * @param   \PhpSpec\Wrapper\Collaborator|UserEntity        $user
+     * @param   \PhpSpec\Wrapper\Collaborator|UserRepository    $userRepository
+     */
+    function it_should_return_expected_value_when_calling_setUser_method_with_user(
+        UserEntity $user,
+        UserRepository $userRepository
+    ) {
+        $userRepository->loadUserByUsername(Argument::any())->shouldNotBeCalled();
+
+        $this->setUser($user)->shouldReturn($this);
     }
 }
