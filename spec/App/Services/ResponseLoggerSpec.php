699cafbb9d78da6fa7ccc58b9ec613ae1cdbf552
tarlepp <tarmo.leppanen@protacon.com>
2016-12-08 22:38:17 +0200

    Moar specs for App\Services\ResponseLogger

--- spec/App/Services/ResponseLoggerSpec.php
+++ spec/App/Services/ResponseLoggerSpec.php
@@ -7,11 +7,14 @@ declare(strict_types = 1);
  */
 namespace spec\App\Services;
 
+use App\Entity\RequestLog as RequestLogEntity;
 use App\Services\Rest\RequestLog as RequestLogService;
 use App\Services\Interfaces\ResponseLogger as ResponseLoggerInterface;
 use App\Services\ResponseLogger;
 use PhpSpec\ObjectBehavior;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class ResponseLoggerSpec
@@ -36,5 +39,39 @@ class ResponseLoggerSpec extends ObjectBehavior
     {
         $this->shouldHaveType(ResponseLogger::class);
         $this->shouldImplement(ResponseLoggerInterface::class);
+    }
+
+    /**
+     * @param   \PhpSpec\Wrapper\Collaborator|Response          $response
+     * @param   \PhpSpec\Wrapper\Collaborator|RequestLogService $service
+     * @param   \PhpSpec\Wrapper\Collaborator|RequestLogEntity  $requestLog
+     */
+    function it_should_do_nothing_if_request_is_not_set(
+        Response $response,
+        RequestLogService $service,
+        RequestLogEntity $requestLog
+    ) {
+        $this->setResponse($response);
+
+        $service->save($requestLog)->shouldNotBeCalled();
+
+        $this->handle();
+    }
+
+    /**
+     * @param   \PhpSpec\Wrapper\Collaborator|Request           $request
+     * @param   \PhpSpec\Wrapper\Collaborator|RequestLogService $service
+     * @param   \PhpSpec\Wrapper\Collaborator|RequestLogEntity  $requestLog
+     */
+    function it_should_do_nothing_if_response_is_not_set(
+        Request $request,
+        RequestLogService $service,
+        RequestLogEntity $requestLog
+    ) {
+        $this->setRequest($request);
+
+        $service->save($requestLog)->shouldNotBeCalled();
+
+        $this->handle();
     }
 }
