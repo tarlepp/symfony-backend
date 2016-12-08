38542036700e1bfbc324fddcd5eec8d8c66094fd
tarlepp <tarmo.leppanen@protacon.com>
2016-12-08 21:14:28 +0200

    Added one more spec for App\Services\LoginLogger

--- spec/App/Services/LoginLoggerSpec.php
+++ spec/App/Services/LoginLoggerSpec.php
@@ -8,6 +8,7 @@ declare(strict_types = 1);
 namespace spec\App\Services;
 
 use App\Entity\User as UserEntity;
+use App\Entity\UserLogin as UserLoginEntity;
 use App\Services\Interfaces\LoginLogger as LoginLoggerInterface;
 use App\Services\LoginLogger;
 use App\Services\Rest\UserLogin as UserLoginService;
@@ -15,6 +16,8 @@ use App\Repository\User as UserRepository;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\HeaderBag;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -51,25 +54,65 @@ class LoginLoggerSpec extends ObjectBehavior
      * @param   \PhpSpec\Wrapper\Collaborator|UserInterface     $user
      * @param   \PhpSpec\Wrapper\Collaborator|UserRepository    $userRepository
      */
-    function it_should_return_expected_value_when_calling_setUser_method_without_user(
+    function it_should_fetch_user_from_repository_when_calling_setUser_method_with_UserInterface(
         UserInterface $user,
         UserRepository $userRepository
     ) {
         $userRepository->loadUserByUsername(Argument::any())->shouldBeCalled()->willReturn($user);
 
-        $this->setUser($user)->shouldReturn($this);
+        $this->setUser($user);
     }
 
     /**
      * @param   \PhpSpec\Wrapper\Collaborator|UserEntity        $user
      * @param   \PhpSpec\Wrapper\Collaborator|UserRepository    $userRepository
      */
-    function it_should_return_expected_value_when_calling_setUser_method_with_user(
+    function it_should_not_fetch_user_from_repository_when_calling_setUser_method_with_UserEntity(
         UserEntity $user,
         UserRepository $userRepository
     ) {
         $userRepository->loadUserByUsername(Argument::any())->shouldNotBeCalled();
 
-        $this->setUser($user)->shouldReturn($this);
+        $this->setUser($user);
+    }
+
+    /**
+     * @param   \PhpSpec\Wrapper\Collaborator|RequestStack      $requestStack
+     * @param   \PhpSpec\Wrapper\Collaborator|Request           $request
+     * @param   \PhpSpec\Wrapper\Collaborator|HeaderBag         $headerBag
+     * @param   \PhpSpec\Wrapper\Collaborator|LoggerInterface   $logger
+     * @param   \PhpSpec\Wrapper\Collaborator|UserEntity        $user
+     * @param   \PhpSpec\Wrapper\Collaborator|UserLoginService  $userLoginService
+     * @param   \PhpSpec\Wrapper\Collaborator|UserLoginEntity   $userLogin
+     */
+    function it_should_log_user_login_as_expected(
+        RequestStack $requestStack,
+        Request $request,
+        HeaderBag $headerBag,
+        LoggerInterface $logger,
+        UserEntity $user,
+        UserLoginService $userLoginService,
+        UserLoginEntity $userLogin
+    ) {
+        // Mock user-agent get
+        $headerBag->get('User-Agent')->shouldBeCalled()->willReturn('fake-user-agent');
+
+        // Mock necessary things in request
+        $request->headers = $headerBag;
+        $request->getClientIp()->willReturn('fake ip');
+        $request->getHost()->willReturn('fake host');
+
+        // Attach mocked request to request stack
+        $requestStack->getCurrentRequest()->willReturn($request);
+
+        // Check that user login save is called
+        $userLoginService->save(Argument::type(UserLoginEntity::class))->shouldBeCalled()->willReturn($userLogin);
+
+        // Logger debug method should be called
+        $logger->debug('Created new login entry to database.')->shouldBeCalled();
+
+        // And do everything necessary
+        $this->setUser($user);
+        $this->handle();
     }
 }
