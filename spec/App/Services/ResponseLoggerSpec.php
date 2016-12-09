2f5b2d79451e31012d5e426884f876d2a66f7e6e
tarlepp <tarmo.leppanen@protacon.com>
2016-12-09 09:16:12 +0200

    One more spec for App\Services\ResponseLogger

--- spec/App/Services/ResponseLoggerSpec.php
+++ spec/App/Services/ResponseLoggerSpec.php
@@ -8,11 +8,14 @@ declare(strict_types = 1);
 namespace spec\App\Services;
 
 use App\Entity\RequestLog as RequestLogEntity;
-use App\Services\Rest\RequestLog as RequestLogService;
+use App\Repository\RequestLog as RequestLogRepository;
 use App\Services\Interfaces\ResponseLogger as ResponseLoggerInterface;
 use App\Services\ResponseLogger;
+use App\Services\Rest\RequestLog as RequestLogService;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\HeaderBag;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -32,7 +35,7 @@ class ResponseLoggerSpec extends ObjectBehavior
         LoggerInterface $logger,
         RequestLogService $service
     ) {
-        $this->beConstructedWith($logger, $service, 'spec');
+        $this->beConstructedWith($logger, $service, 'dev');
     }
 
     function it_is_initializable()
@@ -72,6 +75,60 @@ class ResponseLoggerSpec extends ObjectBehavior
 
         $service->save($requestLog)->shouldNotBeCalled();
 
+        $this->handle();
+    }
+
+    /**
+     * @param   \PhpSpec\Wrapper\Collaborator|Request               $request
+     * @param   \PhpSpec\Wrapper\Collaborator|Response              $response
+     * @param   \PhpSpec\Wrapper\Collaborator|HeaderBag             $headerBag
+     * @param   \PhpSpec\Wrapper\Collaborator|RequestLogService     $service
+     * @param   \PhpSpec\Wrapper\Collaborator|RequestLogRepository  $repository
+     */
+    function it_should_create_new_RequestLog_entity(
+        Request $request,
+        Response $response,
+        HeaderBag $headerBag,
+        RequestLogService $service,
+        RequestLogRepository $repository
+    ) {
+        // Mock HeaderBag call
+        $headerBag->all()->shouldBeCalled()->willReturn([]);
+
+        // Attach mocked HeaderBag to Request
+        $request->headers = $headerBag;
+
+        // Mock all necessary in Request
+        $request->getClientIp()->shouldBeCalled()->willReturn('fake ip');
+        $request->getRealMethod()->shouldBeCalled()->willReturn('fake real method');
+        $request->getScheme()->shouldBeCalled()->willReturn('fake scheme');
+        $request->getHttpHost()->shouldBeCalled()->willReturn('fake http host');
+        $request->getBasePath()->shouldBeCalled()->willReturn('fake base path');
+        $request->getScriptName()->shouldBeCalled()->willReturn('fake script name');
+        $request->getPathInfo()->shouldBeCalled()->willReturn('fake path info');
+        $request->getRequestUri()->shouldBeCalled()->willReturn('fake request uri');
+        $request->getUri()->shouldBeCalled()->willReturn('fake uri');
+        $request->get('_controller', '')->shouldBeCalled()->willReturn('fake::controller');
+        $request->getContentType()->shouldBeCalled()->willReturn('fake content type');
+        $request->getMimeType(Argument::any())->shouldBeCalled()->willReturn('fake mime type');
+        $request->getContent(Argument::any())->shouldBeCalled()->willReturn('fake content');
+        $request->isXmlHttpRequest()->shouldBeCalled()->willReturn(false);
+
+        // Mock all necessary in Response
+        $response->getStatusCode()->shouldBeCalled()->willReturn(200);
+        $response->getContent()->shouldBeCalled()->willReturn('fake content');
+
+        // Mock repository method
+        $repository->cleanHistory()->shouldBeCalled();
+
+        // Request log should be save
+        $service->getRepository()->shouldBeCalled()->willReturn($repository);
+        $service->save(Argument::type(RequestLogEntity::class), Argument::type('bool'))->shouldBeCalled();
+
+        // And finally make actual call
+        $this->setRequest($request);
+        $this->setResponse($response);
+        $this->setMasterRequest(true);
         $this->handle();
     }
 }
