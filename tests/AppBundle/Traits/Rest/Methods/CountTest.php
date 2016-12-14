0ce04a72c2445b99c7a43f3d750ab9a046ac752f
tarlepp <tarmo.leppanen@protacon.com>
2016-12-14 19:24:18 +0200

    Few more test for App\Traits\Rest\Methods\Count trait.

--- tests/AppBundle/Traits/Rest/Methods/CountTest.php
+++ tests/AppBundle/Traits/Rest/Methods/CountTest.php
@@ -8,8 +8,14 @@ declare(strict_types = 1);
 namespace AppBundle\Traits\Rest\Methods;
 
 use App\Traits\Rest\Methods\Count;
+use App\Services\Rest\Helper\Interfaces\Response as RestHelperResponseInterface;
+use App\Services\Rest\Interfaces\Base as ResourceServiceInterface;
+use AppBundle\Traits\Rest\Methods\Count as CountTestClass;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+require_once dirname(__FILE__) . '/Count.php';
 
 /**
  * Class CountTest
@@ -29,5 +35,79 @@ class CountTest extends KernelTestCase
         $request = Request::create('/count', 'GET');
 
         $mock->countMethod($request);
+    }
+
+    public function testThatTraitCallsServiceMethods()
+    {
+        $resourceService = $this->createMock(ResourceServiceInterface::class);
+        $restHelperResponse = $this->createMock(RestHelperResponseInterface::class);
+
+        /** @var CountTestClass|\PHPUnit_Framework_MockObject_MockObject $countTestClass */
+        $countTestClass = $this->getMockForAbstractClass(CountTestClass::class, [$resourceService, $restHelperResponse]);
+
+        // Create request and response
+        $request = Request::create('/count', 'GET');
+        $response = Response::create(123);
+
+        $resourceService
+            ->expects(static::once())
+            ->method('count')
+            ->withAnyParameters()
+            ->willReturn(123);
+
+        $restHelperResponse
+            ->expects(static::once())
+            ->method('createResponse')
+            ->withAnyParameters()
+            ->willReturn($response);
+
+        $countTestClass
+            ->expects(static::once())
+            ->method('getResourceService')
+            ->willReturn($resourceService);
+
+        $countTestClass
+            ->expects(static::once())
+            ->method('getResponseService')
+            ->willReturn($restHelperResponse);
+
+        $countTestClass->countMethod($request)->getContent();
+    }
+
+    /**
+     * @expectedException \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException
+     *
+     * @dataProvider dataProviderTestThatTraitsThrowsAnExceptionWithWrongHttpMethod
+     *
+     * @param   string  $httpMethod
+     */
+    public function testThatTraitsThrowsAnExceptionWithWrongHttpMethod(string $httpMethod)
+    {
+        $resourceService = $this->createMock(ResourceServiceInterface::class);
+        $restHelperResponse = $this->createMock(RestHelperResponseInterface::class);
+
+        /** @var CountTestClass|\PHPUnit_Framework_MockObject_MockObject $countTestClass */
+        $countTestClass = $this->getMockForAbstractClass(CountTestClass::class, [$resourceService, $restHelperResponse]);
+
+        // Create request and response
+        $request = Request::create('/count', $httpMethod);
+
+        $countTestClass->countMethod($request)->getContent();
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderTestThatTraitsThrowsAnExceptionWithWrongHttpMethod(): array
+    {
+        return [
+            ['HEAD'],
+            ['POST'],
+            ['PUT'],
+            ['DELETE'],
+            ['OPTIONS'],
+            ['CONNECT'],
+            ['foobar'],
+        ];
     }
 }
