eb2174674e61cad7ec8dd10102d06eedfe9d06d9
tarlepp <tarmo.leppanen@protacon.com>
2016-12-17 18:45:50 +0200

    Added test.

--- tests/AppBundle/Traits/Rest/Methods/FindTest.php
+++ tests/AppBundle/Traits/Rest/Methods/FindTest.php
@@ -8,8 +8,14 @@ declare(strict_types = 1);
 namespace AppBundle\Traits\Rest\Methods;
 
 use App\Traits\Rest\Methods\Find;
+use App\Services\Rest\Helper\Interfaces\Response as RestHelperResponseInterface;
+use App\Services\Rest\Interfaces\Base as ResourceServiceInterface;
+use AppBundle\Traits\Rest\Methods\Find as FindTestClass;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+require_once dirname(__FILE__) . '/Find.php';
 
 /**
  * Class FindTest
@@ -29,5 +35,42 @@ class FindTest extends KernelTestCase
         $request = Request::create('/', 'GET');
 
         $mock->findMethod($request);
+    }
+
+    public function testThatTraitCallsServiceMethods()
+    {
+        $resourceService = $this->createMock(ResourceServiceInterface::class);
+        $restHelperResponse = $this->createMock(RestHelperResponseInterface::class);
+
+        /** @var FindTestClass|\PHPUnit_Framework_MockObject_MockObject $findTestClass */
+        $findTestClass = $this->getMockForAbstractClass(FindTestClass::class, [$resourceService, $restHelperResponse]);
+
+        // Create request and response
+        $request = Request::create('/', 'GET');
+        $response = Response::create('[]');
+
+        $resourceService
+            ->expects(static::once())
+            ->method('find')
+            ->withAnyParameters()
+            ->willReturn([]);
+
+        $restHelperResponse
+            ->expects(static::once())
+            ->method('createResponse')
+            ->withAnyParameters()
+            ->willReturn($response);
+
+        $findTestClass
+            ->expects(static::once())
+            ->method('getResourceService')
+            ->willReturn($resourceService);
+
+        $findTestClass
+            ->expects(static::once())
+            ->method('getResponseService')
+            ->willReturn($restHelperResponse);
+
+        $findTestClass->findMethod($request);
     }
 }
