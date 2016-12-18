ae6eec1161f717f281bed539190353251791f211
tarlepp <tarmo.leppanen@protacon.com>
2016-12-18 12:50:01 +0200

    Only allow GET within Find method trait.

--- src/App/Traits/Rest/Methods/Find.php
+++ src/App/Traits/Rest/Methods/Find.php
@@ -13,6 +13,7 @@ use App\Services\Rest\Helper\Request as RestHelperRequest;
 use App\Services\Rest\Interfaces\Base as ResourceServiceInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 /**
  * Trait for generic 'Find' action for REST controllers. Trait will add following route definition to your controller
@@ -101,6 +102,10 @@ trait Find
                 'You cannot use App\Traits\Rest\Methods\Find trait within class that does not implement ' .
                 'App\Controller\Interfaces\RestController interface.'
             );
+        }
+
+        if (!in_array($request->getMethod(), ['GET'])) {
+            throw new MethodNotAllowedHttpException(['GET']);
         }
 
         // Determine used parameters
