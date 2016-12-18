85612e3d819d87cc9741c0eea674050dc7d23c44
tarlepp <tarmo.leppanen@protacon.com>
2016-12-18 19:53:49 +0200

    Added tests for App\Traits\Roles\Admin traits.

---
+++ tests/AppBundle/Traits/Roles/Admin/RoleTest.php
@@ -0,0 +1,82 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /tests/AppBundle/Traits/Roles/Admin/RoleTest.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace AppBundle\Traits\Roles\Admin;
+
+use Ramsey\Uuid\Uuid;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class RoleTest
+ *
+ * @package AppBundle\Traits\Roles\Admin
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class RoleTest extends KernelTestCase
+{
+    /**
+     * @dataProvider dataProviderTestThatTraitCallsExpectedMethod
+     *
+     * @param string $className
+     */
+    public function testThatTraitCallsExpectedMethod(string $className, string $method, string $traitMethod, array $parameters)
+    {
+        $stub = $this->getMockForTrait(
+            $className,
+            [],
+            '',
+            true,
+            true,
+            true,
+            [$traitMethod]
+        );
+
+        $stub
+            ->expects(static::once())
+            ->method($traitMethod)
+            ->with(...$parameters);
+
+        $result = call_user_func_array([$stub, $method], $parameters);
+
+        static::assertInstanceOf(Response::class, $result);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderTestThatTraitCallsExpectedMethod(): array
+    {
+        $pattern = dirname(__FILE__) . '/../../../../../src/App/Traits/Rest/Roles/Admin/*.php';
+
+        $iterator = function (string $filename): array {
+            $name = str_replace('.php', '', basename($filename));
+
+            $parameters = [
+                $request = $this->createMock(Request::class),
+            ];
+
+            switch ($name) {
+                case 'Delete':
+                case 'FindOne':
+                case 'Update':
+                    $parameters[] = Uuid::uuid4()->toString();
+                    break;
+            }
+
+            return [
+                'App\\Traits\\Rest\\Roles\\Admin\\' . $name,
+                lcfirst($name),
+                lcfirst($name) . 'Method',
+                $parameters,
+            ];
+        };
+
+        return array_map($iterator, glob($pattern));
+    }
+}
