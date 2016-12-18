8c9bc3fcf5f224e43e2cb39c91b633d3aa53fb5f
tarlepp <tarmo.leppanen@protacon.com>
2016-12-18 17:16:59 +0200

    Added missing tests for Ids trait method.

--- tests/AppBundle/Traits/Rest/Methods/IdsTest.php
+++ tests/AppBundle/Traits/Rest/Methods/IdsTest.php
@@ -8,8 +8,14 @@ declare(strict_types = 1);
 namespace AppBundle\Traits\Rest\Methods;
 
 use App\Traits\Rest\Methods\Ids;
+use App\Services\Rest\Helper\Interfaces\Response as RestHelperResponseInterface;
+use App\Services\Rest\Interfaces\Base as ResourceServiceInterface;
+use AppBundle\Traits\Rest\Methods\Ids as IdsTestClass;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+require_once dirname(__FILE__) . '/Ids.php';
 
 /**
  * Class IdsTest
@@ -29,5 +35,107 @@ class IdsTest extends KernelTestCase
         $request = Request::create('/ids', 'GET');
 
         $mock->idsMethod($request);
+    }
+
+    public function testThatTraitCallsServiceMethods()
+    {
+        $resourceService = $this->createMock(ResourceServiceInterface::class);
+        $restHelperResponse = $this->createMock(RestHelperResponseInterface::class);
+
+        /** @var IdsTestClass|\PHPUnit_Framework_MockObject_MockObject $idsTestClass */
+        $idsTestClass = $this->getMockForAbstractClass(IdsTestClass::class, [$resourceService, $restHelperResponse]);
+
+        // Create request and response
+        $request = Request::create('/ids', 'GET');
+        $response = Response::create('[]');
+
+        $resourceService
+            ->expects(static::once())
+            ->method('getIds')
+            ->withAnyParameters()
+            ->willReturn([]);
+
+        $restHelperResponse
+            ->expects(static::once())
+            ->method('createResponse')
+            ->withAnyParameters()
+            ->willReturn($response);
+
+        $idsTestClass
+            ->expects(static::once())
+            ->method('getResourceService')
+            ->willReturn($resourceService);
+
+        $idsTestClass
+            ->expects(static::once())
+            ->method('getResponseService')
+            ->willReturn($restHelperResponse);
+
+        $idsTestClass->idsMethod($request);
+    }
+
+    /**
+     * @expectedException \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException
+     *
+     * @dataProvider dataProviderTestThatTraitThrowsAnExceptionWithWrongHttpMethod
+     *
+     * @param   string  $httpMethod
+     */
+    public function testThatTraitThrowsAnExceptionWithWrongHttpMethod(string $httpMethod)
+    {
+        $resourceService = $this->createMock(ResourceServiceInterface::class);
+        $restHelperResponse = $this->createMock(RestHelperResponseInterface::class);
+
+        /** @var IdsTestClass|\PHPUnit_Framework_MockObject_MockObject $testClass */
+        $testClass = $this->getMockForAbstractClass(IdsTestClass::class, [$resourceService, $restHelperResponse]);
+
+        // Create request and response
+        $request = Request::create('/ids', $httpMethod);
+
+        $testClass->idsMethod($request)->getContent();
+    }
+
+    public function testThatTraitCallsProcessCriteriaIfItExists()
+    {
+        $resourceService = $this->createMock(ResourceServiceInterface::class);
+        $restHelperResponse = $this->createMock(RestHelperResponseInterface::class);
+
+        /** @var IdsTestClass|\PHPUnit_Framework_MockObject_MockObject $testClass */
+        $testClass = $this->getMockForAbstractClass(
+            IdsTestClass::class,
+            [$resourceService, $restHelperResponse],
+            '',
+            true,
+            true,
+            true,
+            ['processCriteria']
+        );
+
+        // Create request
+        $request = Request::create('/ids', 'GET');
+
+        $testClass
+            ->expects(static::once())
+            ->method('processCriteria')
+            ->withAnyParameters()
+        ;
+
+        $testClass->idsMethod($request)->getContent();
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderTestThatTraitThrowsAnExceptionWithWrongHttpMethod(): array
+    {
+        return [
+            ['HEAD'],
+            ['POST'],
+            ['PUT'],
+            ['DELETE'],
+            ['OPTIONS'],
+            ['CONNECT'],
+            ['foobar'],
+        ];
     }
 }
