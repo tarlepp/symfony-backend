b8cb0f3aeb151c444bb4bff5f14e3d92a89a7bb2
tarlepp <tarmo.leppanen@protacon.com>
2016-12-20 00:42:14 +0200

    POC for bug.

--- src/App/EventListener/ExceptionListener.php
+++ src/App/EventListener/ExceptionListener.php
@@ -120,7 +120,9 @@ class ExceptionListener
         // Within AccessDeniedHttpException we need to hide actual real message from users
         if ($exception instanceof AccessDeniedHttpException) {
             $message = 'Access denied.';
-        } elseif ($exception instanceof DBALException || $exception instanceof ORMException) { // Database errors
+        } elseif ($exception instanceof DBALException
+            || $exception instanceof ORMException
+        ) { // Database errors
             $message = 'Database error.';
         } else {
             $message = $exception->getMessage();
