2be53a820a46161173941e7af4533dd5cab583be
tarlepp <tarmo.leppanen@protacon.com>
2017-01-05 18:27:55 +0200

    Small refactor

--- src/App/Services/Rest/Author.php
+++ src/App/Services/Rest/Author.php
@@ -9,6 +9,7 @@ namespace App\Services\Rest;
 
 use App\Entity\Author as Entity;
 use App\Repository\Author as Repository;
+use Doctrine\Common\Persistence\Proxy;
 
 // Note that these are just for the class PHPDoc block
 /** @noinspection PhpHierarchyChecksInspection */
@@ -20,14 +21,15 @@ use App\Repository\Author as Repository;
  * @package App\Services\Rest
  * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
  *
- * @method  Repository  getRepository(): Repository
- * @method  Entity[]    find(array $criteria = [], array $orderBy = [], int $limit = null, int $offset = null, array $search = []): array
- * @method  null|Entity findOne(string $id, bool $throwExceptionIfNotFound = false)
- * @method  null|Entity findOneBy(array $criteria, array $orderBy = [], bool $throwExceptionIfNotFound = false)
- * @method  Entity      create(\stdClass $data): Entity
- * @method  Entity      save(Entity $entity, bool $skipValidation = false): Entity
- * @method  Entity      update(string $id, \stdClass $data): Entity
- * @method  Entity      delete(string $id): Entity
+ * @method  Repository      getRepository(): Repository
+ * @method  Proxy|Entity    getReference(string $id): Proxy
+ * @method  Entity[]        find(array $criteria = [], array $orderBy = [], int $limit = null, int $offset = null, array $search = []): array
+ * @method  null|Entity     findOne(string $id, bool $throwExceptionIfNotFound = false)
+ * @method  null|Entity     findOneBy(array $criteria, array $orderBy = [], bool $throwExceptionIfNotFound = false)
+ * @method  Entity          create(\stdClass $data): Entity
+ * @method  Entity          save(Entity $entity, bool $skipValidation = false): Entity
+ * @method  Entity          update(string $id, \stdClass $data): Entity
+ * @method  Entity          delete(string $id): Entity
  */
 class Author extends Base
 {
