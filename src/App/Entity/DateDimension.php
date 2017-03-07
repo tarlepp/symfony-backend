237f951b601c3fdf602a2fe5c9ba08263e252960
tarlepp <tarmo.leppanen@protacon.com>
2017-03-07 16:43:15 +0200

    style: Long comments to one line - without this doctrine:migrations:diff will always generate not needed migration file.

--- src/App/Entity/DateDimension.php
+++ src/App/Entity/DateDimension.php
@@ -188,8 +188,7 @@ class DateDimension implements EntityInterface
      *      type="integer",
      *      nullable=false,
      *      options={
-     *          "comment": "ISO-8601 numeric representation of the day of the week; 1 (for Monday) through 7
- (for Sunday)",
+     *          "comment": "ISO-8601 numeric representation of the day of the week; 1 (for Monday) through 7 (for Sunday)",
      *      },
      *  )
      */
@@ -252,8 +251,7 @@ class DateDimension implements EntityInterface
      *      type="integer",
      *      nullable=false,
      *      options={
-     *          "comment": "ISO-8601 week-numbering year. This has the same value as year, except that if the ISO week
- number (W) belongs to the previous or next year, that year is used instead.",
+     *          "comment": "ISO-8601 week-numbering year. This has the same value as year, except that if the ISO week number (W) belongs to the previous or next year, that year is used instead.",
      *      },
      *  )
      */
